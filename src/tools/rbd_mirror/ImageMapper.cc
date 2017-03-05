486b84a745b413740b09af3549ef53eab503e4ff
Mykola Golub <mgolub@mirantis.com>
2017-03-05 22:40:54 +0100

    rbd-mirror A/A: stub implementation of ImageMapper class
    Signed-off-by: Mykola Golub <mgolub@mirantis.com>

---
+++ src/tools/rbd_mirror/ImageMapper.cc
@@ -0,0 +1,85 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+
+#include "common/debug.h"
+#include "common/dout.h"
+#include "common/errno.h"
+#include "ImageMapper.h"
+
+#define dout_context g_ceph_context
+#define dout_subsys ceph_subsys_rbd_mirror
+#undef dout_prefix
+#define dout_prefix *_dout << "rbd::mirror::ImageMapper: " \
+                           << this << " " << __func__ << ": "
+
+namespace rbd {
+namespace mirror {
+
+template <typename I>
+ImageMapper<I>::ImageMapper()
+  : m_lock("rbd::mirror::ImageMapper") {
+}
+
+template <typename I>
+void ImageMapper<I>::update(const ImageIds &image_ids,
+              std::vector<std::string> *images_to_detach,
+              std::vector<std::string> *images_to_attach) {
+  dout(20) << dendl;
+
+  Mutex::Locker locker(m_lock);
+
+  for (auto &it : m_images) {
+    assert(it.second == ATTACHED);
+
+    if (image_ids.find(ImageId(it.first)) != image_ids.end()) {
+      continue;
+    }
+
+    it.second = DETACHING;
+    images_to_detach->push_back(it.first);
+  }
+
+  for (auto &image : image_ids) {
+    auto it = m_images.find(image.global_id);
+    if (it == m_images.end()) {
+      m_images[image.global_id] = ATTACHING;
+      images_to_attach->push_back(image.global_id);
+    } else {
+      assert(it->second == ATTACHED);
+    }
+  }
+}
+
+template <typename I>
+void ImageMapper<I>::attach(const std::string &global_image_id) {
+  dout(20) << "global_image_id=" << global_image_id << dendl;
+
+  Mutex::Locker locker(m_lock);
+
+  auto it = m_images.find(global_image_id);
+
+  assert(it != m_images.end());
+  assert(it->second == ATTACHING);
+
+  it->second = ATTACHED;
+}
+
+template <typename I>
+void ImageMapper<I>::detach(const std::string &global_image_id) {
+  dout(20) << "global_image_id=" << global_image_id << dendl;
+
+  Mutex::Locker locker(m_lock);
+
+  auto it = m_images.find(global_image_id);
+
+  assert(it != m_images.end());
+  assert(it->second == DETACHING);
+
+  m_images.erase(it);
+}
+
+} // namespace mirror
+} // namespace rbd
+
+template class rbd::mirror::ImageMapper<librbd::ImageCtx>;
---
+++ src/tools/rbd_mirror/ImageMapper.h
@@ -0,0 +1,51 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#ifndef RBD_MIRROR_IMAGE_MAPPER_H
+#define RBD_MIRROR_IMAGE_MAPPER_H
+
+#include <map>
+#include <string>
+#include <vector>
+
+#include "common/Mutex.h"
+#include "types.h"
+
+namespace librbd { class ImageCtx; }
+
+namespace rbd {
+namespace mirror {
+
+template <typename ImageCtxT = librbd::ImageCtx>
+class ImageMapper {
+public:
+  static ImageMapper *create() {
+    return new ImageMapper();
+  }
+
+  ImageMapper();
+
+  void update(const ImageIds &image_ids,
+              std::vector<std::string> *images_to_detach,
+              std::vector<std::string> *images_to_attach);
+
+  void attach(const std::string &global_image_id);
+  void detach(const std::string &global_image_id);
+
+private:
+  enum State {
+    ATTACHING,
+    ATTACHED,
+    DETACHING,
+  };
+
+  Mutex m_lock;
+  std::map<std::string, State> m_images;
+};
+
+} // namespace mirror
+} // namespace rbd
+
+extern template class rbd::mirror::ImageMapper<librbd::ImageCtx>;
+
+#endif // RBD_MIRROR_IMAGE_MAPPER_H
