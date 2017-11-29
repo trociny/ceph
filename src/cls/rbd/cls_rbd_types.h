e5776c9a43d8b8ea2b62f4c53d7f484f0d3611b8
Mykola Golub <to.my.trociny@gmail.com>
2017-11-29 21:05:18 +0200

    cls/rbd: add "migration" trash image source
    Signed-off-by: Mykola Golub <to.my.trociny@gmail.com>

--- src/cls/rbd/cls_rbd_types.h
+++ src/cls/rbd/cls_rbd_types.h
@@ -330,8 +330,28 @@ WRITE_CLASS_ENCODER(SnapshotNamespaceOnDisk);
 
 enum TrashImageSource {
   TRASH_IMAGE_SOURCE_USER = 0,
-  TRASH_IMAGE_SOURCE_MIRRORING = 1
+  TRASH_IMAGE_SOURCE_MIRRORING = 1,
+  TRASH_IMAGE_SOURCE_MIGRATION = 2,
 };
+
+inline std::ostream& operator<<(std::ostream& os,
+                                const TrashImageSource& source) {
+  switch (source) {
+  case TRASH_IMAGE_SOURCE_USER:
+    os << "user";
+    break;
+  case TRASH_IMAGE_SOURCE_MIRRORING:
+    os << "mirroring";
+    break;
+  case TRASH_IMAGE_SOURCE_MIGRATION:
+    os << "migration";
+    break;
+  default:
+    os << "unknown (" << static_cast<uint32_t>(source) << ")";
+    break;
+  }
+  return os;
+}
 
 inline void encode(const TrashImageSource &source, bufferlist& bl,
 		   uint64_t features=0)
