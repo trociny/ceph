ee42a41b4bc97060f30738114f8cf4c431e89de3
Mykola Golub <mgolub@suse.com>
2018-10-18 20:47:20 +0300

    pybind/mgr: test rbd perf counters
    Signed-off-by: Mykola Golub <mgolub@suse.com>

--- src/pybind/mgr/osd_perf_query/module.py
+++ src/pybind/mgr/osd_perf_query/module.py
@@ -20,6 +20,13 @@ class OSDPerfQuery(MgrModule):
             "desc": "remove osd perf query",
             "perm": "w"
         },
+        {
+            "cmd": "rbd perf "
+                   "name=pool_id,type=CephInt,req=true "
+                   "name=image_id,type=CephString,req=true",
+            "desc": "test rbd perf",
+            "perm": "r"
+        },
     ]
 
     def handle_command(self, inbuf, cmd):
@@ -29,6 +36,14 @@ class OSDPerfQuery(MgrModule):
         elif cmd['prefix'] == "osd perf query remove":
             self.remove_osd_perf_query(cmd['query_id'])
             return 0, "", "removed query with id " + str(cmd['query_id'])
+        elif cmd['prefix'] == "rbd perf":
+            perf = self.get_all_rbd_perf_counters(cmd['pool_id'], cmd['image_id'])
+            op = self.get_rbd_perf_counter(cmd['pool_id'], cmd['image_id'], 'op')
+            inb = self.get_rbd_perf_counter(cmd['pool_id'], cmd['image_id'], 'inb')
+            outb = self.get_rbd_perf_counter(cmd['pool_id'], cmd['image_id'], 'outb')
+            lat = self.get_rbd_perf_counter(cmd['pool_id'], cmd['image_id'], 'lat')
+            msg = "perf for %d.%s: %s\nop: %s\ninb: %s\noutb: %s\nlat: %s" % (cmd['pool_id'], cmd['image_id'], perf, op, inb, outb, lat)
+            return 0, "", msg
         else:
             raise NotImplementedError(cmd['prefix'])
 
